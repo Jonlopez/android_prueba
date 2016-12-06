4d76a60f839f0ec135f6b26bceaaca2a598dd493
Unknown <jonlopezgarrido@gmail.com>
2016-12-06 11:34:01 +0100

    Initial proyect

---
+++ Entrega_2/app/src/main/java/com/dam/jon/entrega_2/PrincipalMainActivity.java
@@ -0,0 +1,13 @@
+package com.dam.jon.entrega_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_main);
+    }
+}

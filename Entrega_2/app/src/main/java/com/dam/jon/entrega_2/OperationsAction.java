a59d54c0ba276b1a3ba339e66bbb5f80f454c65c
Unknown <jonlopezgarrido@gmail.com>
2016-12-12 22:41:28 +0100

    Lógica de negocio y validaciones completados (Mensajes de codigo incluidos)

--- Entrega_2/app/src/main/java/com/dam/jon/entrega_2/OperationsAction.java
+++ Entrega_2/app/src/main/java/com/dam/jon/entrega_2/OperationsAction.java
@@ -9,54 +9,65 @@ import java.util.Hashtable;
 public class OperationsAction {
 
     private Hashtable<Integer, Integer> hashtable_primos = new Hashtable<Integer, Integer>();
+
     private Integer primo;
 
     public Integer primoByPosicion(Integer pos){
 
-        this.primo = (this.hashtable_primos.size() > 0 && this.hashtable_primos.get(pos)!=null)? this.hashtable_primos.get(pos) : calcularPrimo(pos);
+        //Si la tabla contiene elementos Y el primo buscado se encuentra en ella lo retornamos
+        //Si No comprobamos si es el primer número primo el que ns preguntan y retornamos un 1
+        //Si No calculamos el número primo por la posicion y lo insertamos en la tabla de primos (key, value)
+
+        this.primo = (this.hashtable_primos.size() > 0 && this.hashtable_primos.get(pos)!=null)? this.hashtable_primos.get(pos) : (pos == 1)? this.primo = 1 : calcularPrimo(pos);
 
         return this.primo;
+
     }
 
     private Integer calcularPrimo(Integer pos){
-
-        Integer resultado = 0;
-        int num_evl = 1;
+        //Inicializamos el resultado
+        int resultado = 0;
+        //Capturamos la posicion para poder jugar con ella si sobreescribir
         int posicion = pos;
-        for (int x = 1; x<pos; x++)
-        {
-            if(esPrimo(num_evl)){
-                if(posicion == 0){
-                    resultado = num_evl;
-                    break;
-                }else{
-                 posicion -= 1;
-                }
-
-            }else {
-                if(x+1 == pos)
-                    x -= 1;
-            }
-            num_evl += 1;
+        /* Empezamos a comprobar desde el número 2 ya que el número 1 siempre es primo
+         * Y aunque sepamos que el 2 y 3 tambien son primos, hay que empezar por algun lado
+        */
+        int num_evl = 2;
+        // Miestra la poscion sea Mayor que 1 todavía quedan numeros que comprobar
+        while ( posicion > 1 ){
+            //Si el numero A comprobar es primo UNA POSICIÓN MENOS NOS QUEDA PARA ADIVINAR EL DESEADO
+            if( esPrimo(num_evl) )posicion -= 1;
+            //Mientras no hayamos encontrado el número deseado Incrementamos el número a evaluar en 1 para proseguir
+            if( posicion > 1 )num_evl += 1;
         }
 
-
+        //Si hemos salido de la repitiva significa que ya hemos encontrado nuestro número primo
+        resultado = num_evl;
+        //Después de calcular el numero lo alamacenaremos en nuestra tabla (clave, valor)
+        // donde uestra clave será la posicion para un acceso mas rapido la próxima vez que se solicite
         capturarPrimo(pos, resultado);
-
+        //Retornamos el número primo calculado
         return resultado;
     }
 
     private static boolean esPrimo(int numero){
+        //Establecemos el contador a 2 ya que hemos decidido que el la posicion Uno es el primo 1
         int contador = 2;
+        //Inicializamos la comprobación a verdadero
         boolean primo=true;
+        //Mientras no se diga que el número no es primo y el contador sea diferente que el número
+        // a evaluar seguimos comprobando
         while ((primo) && (contador!=numero)){
-            if (numero % contador == 0)
-                primo = false;
+            //Si el número % contador da de resto 0 no es primo
+            if (numero % contador == 0)primo = false;
+            //Incrementamos el contador para la siguiente comprobacion
             contador++;
         }
+        //Devolvemos true o false si el numero a evaluar es primo o no
         return primo;
     }
 
+    //Introducimos el nuevo número primo calculado en la tabla de número primos
     private void capturarPrimo (Integer pos, Integer primo){
         hashtable_primos.put(pos, primo);
     }
--- Entrega_2/app/src/main/java/com/dam/jon/entrega_2/PrincipalMainActivity.java
+++ Entrega_2/app/src/main/java/com/dam/jon/entrega_2/PrincipalMainActivity.java
@@ -1,5 +1,6 @@
 package com.dam.jon.entrega_2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,18 +9,22 @@ import android.widget.TextView;
 
 public class PrincipalMainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Objeto que vamos a usar para realizar las operaciones y conservar los datos en RAM
     private OperationsAction operations;
+    //Boton de la pantalla
     private Button btn_calcular;
+    //Campo donde nos indicaran la posicion
     private TextView tvw_posicion;
+    //Consola de mensajes
     private TextView tvw_resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal_main);
-
+        //Instanciamos el objeto nada mas arrancar la aplicacion
         operations = new OperationsAction();
-
+        //Al igual que el resto de elementos indicando el Listener(escuchador) del boton que vamos a presionar
         btn_calcular = (Button) findViewById(R.id.app_btn);
         btn_calcular.setOnClickListener(this);
         tvw_posicion = (TextView) findViewById(R.id.app_valor);
@@ -28,10 +33,54 @@ public class PrincipalMainActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View view) {
-        tvw_resultado.setText(
-                "El numero primo de la poscion " + tvw_posicion.getText().toString()
-                + " es: "
-                + operations.primoByPosicion(Integer.parseInt(tvw_posicion.getText().toString())).toString()
-        );
+        //Información que vamos a mostrar al usuario para guiarle por el proceso y el resultado final.
+        String infor = "";
+        /*Validaciones:
+            -> Comprobamos que el campo posicion no este vacio
+            -> Comprobamos que el campo posicion no tengas mas de 6 digitos
+            -> Comprobamos que el campo posicion no sea negativo
+                  ( Tengo que indicar que la ultima validacion no es necesaria ya que el campo tvw_posicion
+                    es de tipo inputType : NUMBER y esto evita, valores negativos, caracteres especiales, letras
+                    y que sean números enteros )
+         */
+
+        if( !tvw_posicion.getText().toString().equals("")
+                && Integer.parseInt(tvw_posicion.getText().toString()) >= 0
+                && tvw_posicion.getText().toString().length() <= 6){
+
+            /* Si superamos la validaciones mostraremos en color NEGRO el número primo de la poscion indicada
+             * ordenados de menor a mayor
+             */
+
+            //No tiene sentido pero el enunciado admite 0 como posicion
+            // he decidido cortarlo por lo sano
+            if(Integer.parseInt(tvw_posicion.getText().toString()) != 0){
+                infor += "El numero primo de la poscion "
+                        + tvw_posicion.getText().toString()
+                        + " es: "
+                        + operations.primoByPosicion(Integer.parseInt(tvw_posicion.getText().toString())).toString();
+                tvw_resultado.setTextColor(Color.BLACK);
+            }else{
+                infor += "El primo eres tú xD...";
+                tvw_resultado.setTextColor(Color.BLUE);
+            }
+
+
+        }else{
+            /* Por el contrario si no supera la validaciones indicaremos al usuario un mensaje en ROJO
+             * con lo que tiene que corregir para poder realizar las operaciones necesarias
+             */
+            if(tvw_posicion.getText().toString().equals(""))
+                infor += "El campo posicion no puede estar vacio";
+            else if(tvw_posicion.getText().toString().length() > 6)
+                infor += "La posicion introducida no puede ser de mas de 6 digitos ";
+            else if(Integer.parseInt(tvw_posicion.getText().toString()) < 0)
+                infor += "La posición no puede ser negativa";
+
+            tvw_resultado.setTextColor(Color.RED);
+        }
+        //Finalmente mostraremos el mensaje de informacion que corresponda suceda lo que suceda
+        tvw_resultado.setText(infor);
+
     }
 }
